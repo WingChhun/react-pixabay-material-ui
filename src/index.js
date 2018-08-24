4fdba0e670468460e9b5c8f870ccfb3922651b1f
WingChhun <capt.wing.chhun@gmail.com>
2018-08-23 17:38:44 -0700

    initial

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+const API_KEY = "9909011-31c4fbf54d841a83ce0e1cfa5";
+
+ReactDOM.render(
+    <App/>, document.getElementById('root'));
+registerServiceWorker();
