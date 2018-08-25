a4aabcefa6aab454fe6f147e4838f8ca86c66979
WingChhun <capt.wing.chhun@gmail.com>
2018-08-25 13:13:14 -0700

    Modify SearchBar, add handleChange function

--- src/components/search/SearchBar.js
+++ src/components/search/SearchBar.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 
 import TextField from 'material-ui/TextField';
 import SelectField from 'material-ui/SelectField';
+import MenuItem from 'material-ui/MenuItem';
 import {API_KEY} from "../../constants.js";
 
 class Search extends Component {
@@ -20,13 +21,57 @@ class Search extends Component {
         }
     }
 
+    //todo: handle Changes, use partial function syntax to prevent re-render
+    handleChange = field => event => {
+
+        const {searchText, amount} = this.state;
+        switch (field) {
+
+            case "amount":
+                console.log("amount ", field);
+
+                break;
+
+            case "search":
+                console.log('search', field);
+                break;
+
+            default:
+
+                return;
+        }
+
+    }
+
     render()
     {
+
+        const {searchText, amount} = this.state;
+
         return (
             <div>
 
-                <TextField/>
-                <SelectField/>
+                <TextField
+                    name={'searchText'}
+                    value
+                    ={searchText}
+                    onChange={this.handleChange("search")}
+                    fullWidth
+                    floatingLabelText="Search For Images"/>
+
+                <br/>
+                <SelectField
+                    name="amount"
+                    value={amount}
+                    floatingLabelText="Amount"
+                    onChange={this.handleChange("amount")}>
+
+                    <MenuItem value ={1} primaryText="Hello"/>
+                    <MenuItem value ={1} primaryText="Hello"/>
+                    <MenuItem value ={1} primaryText="Hello"/>
+                    <MenuItem value ={1} primaryText="Hello"/>
+                    <MenuItem value ={1} primaryText="Hello"/>
+                </SelectField>
 
             </div>
         )
