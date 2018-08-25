55be560ddd4f3d99abe57e199df32d69912f8431
WingChhun <capt.wing.chhun@gmail.com>
2018-08-25 13:05:50 -0700

    Initialize constants.js with API_KEY and index.js remove css

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-const API_KEY = "9909011-31c4fbf54d841a83ce0e1cfa5";
+
 
 ReactDOM.render(
     <App/>, document.getElementById('root'));
